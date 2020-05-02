93270fdf5ea80b27b85220cb63aea40f128e575d
Ihor Tsykhovskyi <igorppok@gmail.com>
2020-05-02 19:35:16 +0300

    add prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TelemetryComponent } from './page/telemetry/telemetry.component';
-import { MatGridListModule } from "@angular/material/grid-list";
-import {MatSliderModule} from "@angular/material/slider";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TelemetryComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatGridListModule,
-    MatSliderModule
-  ],
+  declarations: [AppComponent, TelemetryComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatGridListModule, MatSliderModule],
   providers: [],
+
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
